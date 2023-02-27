82ca211844127ea756d1bc7daf032f2f5093d2c2
Cloud User <ec2-user@ip-172-31-48-62.ec2.internal>
2023-02-27 23:03:47 +0000

    i came again waiting for the brethren

--- if2.sh
+++ if2.sh
@@ -3,6 +3,7 @@
 #benjamin was here
 =======
 # Theo was also here
+# Benjamin came here again waiting to see Isaac patrick and Emmanuel
 >>>>>>> d40e2a4ca9118246bd7697f326a6822edece6e32
 #
 # Write a shell script to print numbers from 100-90 
