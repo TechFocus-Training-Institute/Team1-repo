f20f7ad21b8aac84c5dc5c4238cd4c5d1c578852
Cloud User <ec2-user@ip-172-31-48-62.ec2.internal>
2023-02-27 23:10:59 +0000

    i came again waiting for the brethren there was error so i come again

--- if2.sh
+++ if2.sh
@@ -4,6 +4,7 @@
 =======
 # Theo was also here
 # Benjamin came here again waiting to see Isaac patrick and Emmanuel
+#if you see icloud user it is me Benjamin i forgot to sign in with my username and email..... shebang!!
 >>>>>>> d40e2a4ca9118246bd7697f326a6822edece6e32
 #
 # Write a shell script to print numbers from 100-90 
