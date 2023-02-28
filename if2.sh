0e7049be9aaa0c097332c1a0c4ee3c7be7953b7f
Emmanuel Mortoo <emortoo@yahoo.com>
2023-02-28 12:09:04 -0500

    Appended a new line of comment

--- if2.sh
+++ if2.sh
@@ -5,6 +5,8 @@
 # Theo was also here
 # Benjamin came here again waiting to see Isaac patrick and Emmanuel
 #if you see icloud user it is me Benjamin i forgot to sign in with my username and email..... shebang!!
+#i see you brother Ben, we are still waiting for the others to update the file.
+
 >>>>>>> d40e2a4ca9118246bd7697f326a6822edece6e32
 #
 # Write a shell script to print numbers from 100-90 
